04a26bf480f852158370e4ad0cceab9e0f740003
AndreaCatapano <a.catapano96@gmail.com>
2025-03-04 17:00:38 +0100

    refactor: refactored code in bonus-2

--- bonus-2/js/main.js
+++ bonus-2/js/main.js
@@ -6,30 +6,43 @@ let counterNumber = 0;
 
 
 
-function toggleLight (){
-   if (lamp.src.includes("white_lamp.png")) {
-      lamp.src = "img/yellow_lamp.png";
-      button.textContent = "Premi per spegnere";
-      button.classList.replace("button-on", "button-off");
-      container.classList.replace("bg-color-dark", "bg-color-yellow");
-      counter.classList.replace("button-on", "button-off");
-  } else {
-      lamp.src = "img/white_lamp.png";
-      button.textContent = "Premi per accendere";
-      button.classList.replace("button-off", "button-on");
-      container.classList.replace("bg-color-yellow", "bg-color-dark");
-      counter.classList.replace("button-off", "button-on");
-  }
-  if (counterNumber <= 10){
-  counterNumber++;
-  counter.textContent = counterNumber;
-  } else {
-    button.outerHTML = "<p class='button button-on'>HAI ROTTO LA LAMPADINA</p>";
-    counter.textContent = "??";
-    container.classList.replace("bg-color-dark", "bg-color-warning");
-  }
+function toggleLight() {
+    if (lamp.src.includes("white_lamp.png")) {
+        turnOn()
+    } else {
+        turnOff()
+    }
+    addAnUse() 
 }
 
 button.addEventListener("click", toggleLight);
+
+
+function turnOn() {
+    lamp.src = "img/yellow_lamp.png";
+    button.textContent = "Premi per spegnere";
+    button.classList.replace("button-on", "button-off");
+    container.classList.replace("bg-color-dark", "bg-color-yellow");
+    counter.classList.replace("button-on", "button-off");
+}
+
+function turnOff() {
+    lamp.src = "img/white_lamp.png";
+    button.textContent = "Premi per accendere";
+    button.classList.replace("button-off", "button-on");
+    container.classList.replace("bg-color-yellow", "bg-color-dark");
+    counter.classList.replace("button-off", "button-on");
+}
+
+function addAnUse() {
+    if (counterNumber <= 10) {
+        counterNumber++;
+        counter.textContent = counterNumber;
+    } else {
+        button.outerHTML = "<p class='button button-on'>HAI ROTTO LA LAMPADINA</p>";
+        counter.textContent = "??";
+        container.classList.replace("bg-color-dark", "bg-color-warning");
+    }
+}
 
 
