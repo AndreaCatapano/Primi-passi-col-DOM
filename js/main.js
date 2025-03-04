c5e8a40db24132cd5e655ed2412de5cc923bce08
AndreaCatapano <a.catapano96@gmail.com>
2025-03-04 14:54:47 +0100

    feat: added element in index and writed code in main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const lamp = document.getElementById(`lamp`);
+const button = document.querySelector("button");
+
+
+
+function lightOn (){
+   lamp.src = "img/yellow_lamp.png"; 
+}
+
+button.addEventListener("click", lightOn);
