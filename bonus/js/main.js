b2d0ddba5211ba001f1d51e71977c346272d82fd
AndreaCatapano <a.catapano96@gmail.com>
2025-03-04 15:09:21 +0100

    feat: completed bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,18 @@
+const lamp = document.getElementById("lamp");
+const button = document.querySelector("button");
+
+
+
+function toggleLight (){
+   if (lamp.src.includes("white_lamp.png")) {
+      lamp.src = "img/yellow_lamp.png";
+      button.textContent = "Premi per spegnere"
+  } else {
+      lamp.src = "img/white_lamp.png";
+      button.textContent = "Premi per accendere"
+  }
+}
+
+button.addEventListener("click", toggleLight);
+
+
